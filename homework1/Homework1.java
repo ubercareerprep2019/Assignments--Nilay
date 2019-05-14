8222790261d0e7a205c6fb87ae43c9dc15fe681a
nilay1808 <nilay18sadavarte@gmail.com>
2019-05-14 17:34:59 -0400

    Part 3 Stacks
    Implemented a stack using an Array.

--- homework1/Homework1.java
+++ homework1/Homework1.java
@@ -12,31 +12,47 @@ public class Homework1 {
 		/*
 		 * 		Custom test space for the 2 functions.
 		 */
-
-		String s1 = "";		// Fill in with desired string
-		String s2 = "";		// Fill in with desired string
-
-		if(isStringPermutation(s1, s2)) {
-			System.out.println("The 2 strings are permutations!");
+		
+//		String s1 = "";		// Fill in with desired string
+//		String s2 = "";		// Fill in with desired string
+//		
+//		if(isStringPermutation(s1, s2)) {
+//			System.out.println("The 2 strings are permutations!");
+//		}
+//		else {
+//			System.out.println("The 2 strings are not permutations :(");
+//		}
+//		
+//
+//		System.out.println("\n------------------------------------------------------------------- \n");
+//		
+//		
+//		int[] arr = IntStream.range(-3, 14).toArray();				// Fill in numbers
+//		int target = 5;											// Set a target number
+//		
+//		ArrayList<int[]> list = pairsThatEqualSum(arr, target);
+//		
+//		for(int[] a : list) {
+//			System.out.println(a[0] + " + " + a[1]);
+//		}
+		
+		
+		Stack<Integer> stack = new Stack<Integer>();
+		
+		for(int i = 1; i <= 10; i++) {
+			stack.push(i);
 		}
-		else {
-			System.out.println("The 2 strings are not permutations :(");
-		}
-
-
-		System.out.println("\n------------------------------------------------------------------- \n");
-
-
-		int[] arr = IntStream.range(-3, 14).toArray();				// Fill in numbers
-		int target = 5;											// Set a target number
-
-		ArrayList<int[]> list = pairsThatEqualSum(arr, target);
-
-		for(int[] a : list) {
-			System.out.println(a[0] + " + " + a[1]);
-		}
-
-
+		System.out.println(stack.isFull());
+		stack.push(19);
+		stack.push(29);
+		stack.push(39);
+		System.out.println(stack.pop());
+		System.out.println(stack.isFull());
+		stack.pop();
+		System.out.println(stack.top());
+		stack.push(99);
+		System.out.println(stack.top());
+		
 
 	}
 
@@ -48,23 +64,23 @@ public class Homework1 {
 	 * 		The function uses 2 HashMaps to store the frequencies from each of the input strings and a HashSet to store all the unique characters (needed to interate over the 2 HashMaps in the end).
 	 * 		The function basically calculates the frequency of each unique character from the strings and then returns true only if the frequencies match for every character.
 	 * 		The runtime for this function should be O(n).
-	 *
+	 * 
 	 * 		Following assumptions made for this program-
 	 * 			- The strings can contain any character including numbers and other characters.
 	 * 			- The function is case sensitive, meaning that it will look at 'A' and 'a' differently and hence they won't be a valid permutation.
 	 * 			- The function will consider 2 strings to be valid permutations only if they have the same length.
 	 */
 	public static boolean isStringPermutation(String s1, String s2) {
-
+		
 		// The assumption I made was that a string "abc" and "abcc" are not valid permutations.
 		if (s1.length() != s2.length()) {
 			return false;
 		}
-
+		
 		// Using a HashMap to store the characters and their frequencies for each string
 		HashMap<Character, Integer> map1 = new HashMap<Character, Integer>();
 		HashMap<Character, Integer> map2 = new HashMap<Character, Integer>();
-
+		
 		// Using a HashSet to store all unique characters
 		HashSet<Character> set = new HashSet<Character>();
 
@@ -75,7 +91,7 @@ public class Homework1 {
 				map1.put(c, i++);
 			}
 			else {
-				 map1.put(c, 0);
+				 map1.put(c, 1);
 			}
 			set.add(c);
 		}
@@ -87,7 +103,7 @@ public class Homework1 {
 				map2.put(c, i++);
 			}
 			else {
-				 map2.put(c, 0);
+				 map2.put(c, 1);
 			}
 			set.add(c);
 		}
@@ -98,30 +114,30 @@ public class Homework1 {
 				return false;
 			}
 		}
-
+		
 		return true;
 	}
 
-
-
+	
+	
 	/*
 	 * 		The function first transfers all the integers from the array to a Hashtable and computes each integers frequency.
 	 * 		For a given n, it checks if there exists a number 'target-n' (also makes sure for cases where n = target-n and there is only 1 occurance of n)
 	 * 		If it can find a valid pair, it adds both in a list which is returned. The algorithm should run in O(n) time.
-	 *
+	 * 
 	 * 		Assumptions made-
 	 * 			- The array might contains multiple occurances of the same number.
 	 * 			- The output list should only consider unique equations (eg- 1 + 2 and 2 + 1 are the same and not unique)
-	 *
+	 * 	 		
 	 */
-	public static ArrayList<int[]> pairsThatEqualSum(int[] arr, int target) {
-
+	public static ArrayList<int[]> pairsThatEqualSum(int[] arr, int target) {	
+		
 		// Using a hashtable to store the values and their frequencies from input array/
 		Hashtable<Integer, Integer> hash = new Hashtable<Integer, Integer>();
-
+		
 		// A list to store all the valid pairs.
 		ArrayList<int[]> list = new ArrayList<int[]>();
-
+		
 		// Populates the hashtable with respective values and frequencies
 		for(int n : arr) {
 			if(hash.containsKey(n)) {
@@ -132,15 +148,15 @@ public class Homework1 {
 				hash.put(n, 1);
 			}
 		}
-
+		
 		// for every value from the hashtable, checks if there is another value which can add up to the target.
 		for(int n : arr) {
-
-			// Gets the frequency and reduces it by 1 to make sure it doesn't use the same number twice unless it exists multiple times.
+			
+			// Gets the frequency and reduces it by 1 to make sure it doesn't ust the same number twice unless it exists multiple times.
 			int freq = hash.get(n);
 			hash.replace(n, freq-1);
-
-			// If there is a number (target-n) that exists in the table, then we have a successful pair.
+			
+			// If there is a number (target-n) that exists in the table such that it isn't the number we're looking at right now, then we have a successful pair.
 			if(hash.containsKey(target-n) && hash.get(target-n) > 0) {
 				int[] a = {n, target-n};
 				list.add(a);
@@ -150,8 +166,74 @@ public class Homework1 {
 				hash.replace(n, freq);
 			}
 		}
-
+		
 		return list;
+	}
+
+}
+
+
+
+
+/*
+ * Part 3: Stacks & Queues
+ */
+
+class Stack<T> {
+
+	//private ArrayList<T> stack;
+	private int defSize = 10;
+	
+	private T[] arr;
+	private int top;
+	
+
+	public Stack() {
+		arr = (T[]) new Object[defSize];
+		top = -1;
+	}
+
+	public Stack(int size) {
+		defSize = size;
+		arr = (T[]) new Object[defSize];
+		top = -1;
+	}
+
+	public void push(T element) {
+		
+		if(this.isFull()) {
+			T[] temp = (T[]) new Object[defSize*2];
+			for(int i = 0; i <= top; i++) {
+				temp[i] = arr[i];
+			}
+			arr = temp;
+			defSize = defSize*2;
+		}
+		
+		arr[++top] = element;
+	}
+
+	public T pop() {
+		if(top == -1) {
+			System.out.println("Error: Stack is empty!");
+			return null;
+		}
+		return arr[top--];
+	}
+
+	public T top() {
+		if(top == -1) {
+			return null;
+		}
+		return arr[top];
+	}
+
+	public boolean isEmpty() {
+		return top == -1;
+	}
+	
+	public boolean isFull() {
+		return top >= (defSize-1);
 	}
 
 }
